2bd177f56ed649dce440024a45e1171bdfd044d3
AhmedEhab2020 <am8122004@gmailcom>
2023-02-17 09:52:02 -0800

    print all digit number of base 10 start from 0 by putchar function

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all base 10 numbers with putchar function
+ *
+ * Return: Always 0 (sucsses)
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		putchar('0'+ n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
