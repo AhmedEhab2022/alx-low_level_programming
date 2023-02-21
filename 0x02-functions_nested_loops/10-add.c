d66f611aa6dd96a8de9f101ea6847266f373fc01
AhmedEhab2020 <am8122004@gmailcom>
2023-02-21 12:01:08 -0800

    Display the result of add two integer numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - add two integers and returns the result
+ *
+ * @n1: the first number
+ * @n2: the second number
+ *
+ * Return: the result of addition
+ */
+int add(int n1, int n2)
+{
+	return ((n1 + n2));
+}
