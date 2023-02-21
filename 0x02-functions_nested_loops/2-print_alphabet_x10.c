e055bc7a8e1225367f6a82a447edfd4b39988ed8
AhmedEhab2020 <am8122004@gmailcom>
2023-02-21 06:45:59 -0800

    Display all lowercase alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Display all lowercase alphabet 10 times
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+			_putchar(c);
+	}
+	_putchar('\n');
+}
