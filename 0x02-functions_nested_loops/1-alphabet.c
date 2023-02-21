b28cfb534f9566c3beec2ad7f3fc89f852f776d2
AhmedEhab2020 <am8122004@gmailcom>
2023-02-21 06:36:20 -0800

    display all lowercase alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Display all lowercase alphabet
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
