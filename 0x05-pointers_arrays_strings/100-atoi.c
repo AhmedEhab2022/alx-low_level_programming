8cdd06e5bb69822350047012c90deb82ade2269d
AhmedEhab2020 <am8122004@gmailcom>
2023-02-28 13:50:14 -0800

    convert a string number to an integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * _atoi - Convert a string to an integer
+ *
+ * @s: The string
+ *
+ * Return: Always 0 (sucess)
+ */
+int _atoi(char *s)
+{
+	int i, len, num, neg_count;
+
+	len = _strlen(s);
+	num = 0;
+	neg_count = 0;
+	for (i = 0; i < len; i++)
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			num = num * 10 + (s[i] - 48);
+		}
+		else if (s[i] == '-' && num == 0)
+			neg_count++;
+		else if (!(s[i] >= '0' && s[i] <= '9') && (num > 0))
+			break;
+	}
+	if (neg_count % 2 != 0)
+		num = num * -1;
+	return (num);
+}
