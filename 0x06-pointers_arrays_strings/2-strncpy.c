72bc00cfeed2a0867ad079e81d077bc9719c32c7
AhmedEhab2020 <am8122004@gmailcom>
2023-03-02 11:06:43 -0800

    function that copies a string like strncpy bult-in function in C

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,8 +14,8 @@ char *_strncpy(char *dest, char *src, int n)
 	int i;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
-			dest[i] = src[i];
-	if (src[i] == '\0' && i < n )
+		dest[i] = src[i];
+	if (src[i] == '\0' && i < n)
 	{
 		for (; i < n; i++)
 			dest[i] = '\0';
