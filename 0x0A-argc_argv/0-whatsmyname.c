d9ae9c70effb583bbed9f24f114c4d345ab34c0a
AhmedEhab2020 <am8122004@gmailcom>
2023-03-10 05:37:35 -0800

    Task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,9 +9,9 @@
  *
  * Return: Always 0 (sucsses)
  */
-int main(int argc ,char *argv[])
+int main(int argc, char *argv[])
 {
-	if(argc >= 1)
+	if (argc >= 1)
 		printf("%s\n", argv[0]);
 	return (0);
 }
