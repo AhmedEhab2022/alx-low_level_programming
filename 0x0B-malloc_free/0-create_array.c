00beaa4544a1f8fc9a318b507d714e4767ddcc64
AhmedEhab2020 <am8122004@gmailcom>
2023-03-13 04:12:49 -0700

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -23,6 +23,12 @@ char *create_array(unsigned int size, char c)
 
 	p = malloc(size * sizeof(char));
 
+	if (p == NULL)
+	{
+		p = '\0';
+		return (p);
+	}
+
 	for (i = 0; i < size; i++)
 		p[i] = c;
 
