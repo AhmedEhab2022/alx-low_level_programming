9ce4ad644361867fc07f2537b76ab9bef1a4530c
AhmedEhab2020 <am8122004@gmailcom>
2023-03-22 09:39:53 -0700

    Fix betty fails

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -3,7 +3,7 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator - Executes a function given as 
+ * array_iterator - Executes a function given as
  * a parameter on each element of an array.
  *
  * @array: The array of integers numbers.
@@ -12,7 +12,7 @@
  **/
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	long unsigned int i;
+	unsigned int i;
 
 	if (array != NULL && action != NULL)
 	{
