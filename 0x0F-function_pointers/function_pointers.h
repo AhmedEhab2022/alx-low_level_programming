1166fb636443430f30b88e7d50fe3d5f94525ac6
AhmedEhab2020 <am8122004@gmailcom>
2023-03-22 09:01:32 -0700

    Edit on Header File

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,5 @@
-#include <stddef.h>
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
+#ifndef FUNCPOI_H
+#define FUNCPOI_H
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
