66d2e4fdf6aeab7c44dd9c1ca47b3cfdf6bf828e
AhmedEhab2020 <am8122004@gmailcom>
2023-03-27 03:08:59 -0700

    Upload Header File

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int my_abs(int n);
+void my_reverse(char *s);
+char *my_itoa(int n, char s[]);
+int my_putchar(char c);
+void my_printf(const char *str, ...);
+int _printf(const char *format, ...);
+
+#endif
