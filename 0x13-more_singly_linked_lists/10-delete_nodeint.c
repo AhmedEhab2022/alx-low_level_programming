9b4d0adddb0486004b54e90802f195f85588d164
AhmedEhab2020 <am8122004@gmailcom>
2023-04-04 10:55:29 -0700

    Deletes the node at index index of a listint_t linked list.

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -22,7 +22,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (index != 0)
 	{
 		for (; i < index - 1 && tmp != NULL; i++)
-		{	
+		{
 			if (tmp == NULL)
 				return (-1);
 
@@ -31,8 +31,9 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 	if (index == 0)
 	{
-		*head = tmp->next;
-		free(tmp);
+		req_node = (*head)->next;
+		free(head);
+		*head = req_node;
 		return (1);
 	}
 
