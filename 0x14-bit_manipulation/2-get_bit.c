9408228df72bb34fe00dac18f3693432b8293c2d
AhmedEhab2020 <am8122004@gmailcom>
2023-04-07 07:16:46 -0700

    Returns the value of a bit at a given index.

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,29 +7,11 @@
  * @n: The decimal number.
  * @index: The index, starting from 0 of the bit you want to get.
  *
- * Return: The value of the bit at index index or -1 if an error occured.
+ * Return: The value of the bit at index "index" or -1 if an error occured.
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	char *bin;
-	unsigned int i = 0;
-
-	if (index >= sizeof(unsigned long int) * 8)
-	{
-		return (-1);
-	}
-	bin = malloc(sizeof(char) * n);
-
-	if (bin == NULL)
-		return (-1);
-
-	while (n > 0)
-	{
-		bin[i] = (n % 2) + '0';
-		n /= 2;
-		i++;
-	}
-	if (bin[index] == '0')
+	if ((n & (1 << index)) == 0)
 		return (0);
 	else
 		return (1);
