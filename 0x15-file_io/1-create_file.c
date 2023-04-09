78903340d849a52ea8f920e65b07765bbe3a028c
AhmedEhab2020 <am8122004@gmailcom>
2023-04-09 08:24:24 -0700

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,8 +17,11 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	while (text_content[i] != '\0')
-		i++;
+	if (text_content != NULL)
+	{
+		while (text_content[i] != '\0')
+			i++;
+	}
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
